d50b8a813d2f9a4906068cd82c3c4863d5fca19d
Pavlo Bubes <pavlo.bubes@bitsorchestra.com>
2023-03-14 18:19:12 +0200

    init commit

---
+++ AzureWebAppTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureWebAppTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
